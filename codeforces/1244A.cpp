ed782b75bf74a269cc85bfcb07c87bdf6a8a4dd6
RaOne009 <rs1257933@gmail.com>
2019-10-16 22:37:51 +0530

    Updated

---
+++ codeforces/1244A.cpp
@@ -0,0 +1,42 @@
+/*
+RAone00
+Rohit Sharma
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair < ll, ll > pll;
+typedef vector < vector < ll > > matrix;
+typedef vector < ll > vll;
+
+#define pb push_back
+#define debug(x) cout << (#x) << " is " << (x) << endl
+#define fast_io() ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+const ll mod = 1e9 + 7;
+const ll inf = LLONG_MAX;
+const ll N = 1e5 + 10;
+
+void solve() {
+int a, s, d, f, g, tot;
+cin >> a >> s >> d >> f >> g;
+tot = ((a + d - 1) / d) + ((s + f - 1) / f);
+if (tot <= g) {
+    cout << ((a + d - 1) / d) << " " << ((s + f - 1) / f) << "\n";
+} else {
+    cout << "-1\n";
+}
+}
+
+int main() {
+    fast_io();
+    ll tc;
+    cin >> tc;
+    for (ll i = 1; i <= tc; ++i) {
+        solve();
+    }
+    return 0;
+}

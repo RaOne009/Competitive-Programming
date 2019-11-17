1225a5e635ba0aedfb34e4c034b437d44273673b
RaOne009 <rs1257933@gmail.com>
2019-11-17 16:10:51 +0530

    Modified

---
+++ codeforces/1253A.cpp
@@ -0,0 +1,43 @@
+/*
+RAone00
+Rohit sharma
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+#define endl "\n"
+#define debug(x) cout << (#x) << " is " << (x) << endl
+#define fast_io() ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+const ll mod = 1e9 + 7;
+const ll inf = LLONG_MAX;
+const ll N = 1e5 + 10;
+
+bool compare(int q, int w) {
+	return(q > w);
+}
+
+int main() {
+    fast_io();
+    int q;
+    cin >> q;
+    while (q--) {
+		int n;
+		cin >> n;
+		int a[n], b[n];
+		vector < int > v;
+		for (int i = 0; i < n; ++i) {
+			cin >> a[i] >> b[i];
+			if (a[i] != b[i]) {
+				int temp ;
+				
+			} else n = n + 0;
+		}
+		
+	}
+    return 0;
+}
+

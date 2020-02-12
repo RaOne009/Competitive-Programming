bb04509a4301175fbd210013c1b68b83f5a5337f
Rohit Sharma <rs1257933@gmail.com>
2020-02-13 00:33:50 +0530

    initial commit

---
+++ harrypy/faulty_calculator.py
@@ -0,0 +1,20 @@
+num1 = int(input("Enter The first no :"))
+num2 = int(input("Entre the 2nd no :"))
+print("select from this /, *, +, -")
+num3 = input("enter the operater :")
+if num1 == 56 and num2 == 6 and num3 == "/":
+    print("Output :4")
+elif num3 == "/":
+    print("Output :",num1 / num2)
+if num1 == 25 and num2 == 77 and num3 == "*":
+    print("Output :40")
+elif num3 == "*":
+    print("Output :",num1 * num2)
+if num1 == 56 and num2 == 6 and num3 == "+":
+    print("Output :4")
+elif num3 == "+":
+    print("Output :",num1 + num2)
+if num1 == 56 and num2 == 6 and num3 == "-":
+    print("Output :4")
+elif num3 == "-":
+    print("Output :",num1 - num2)    

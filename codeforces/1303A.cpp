d8aaa9bf1adca2156b97d784d12159e26592c4c1
Rohit Sharma <rs1257933@gmail.com>
2020-02-15 23:24:31 +0530

    modified

--- codeforces/1303A.cpp
+++ codeforces/1303A.cpp
@@ -1,1 +1,59 @@
-#include(bits/stdc++.h)
+	/*
+	RAone00
+	Rohit sharma
+	*/
+
+	#include <bits/stdc++.h>
+	using namespace std;
+
+	typedef long long ll;
+	typedef long double ld;
+	typedef pair < ll, ll > pll;
+	typedef vector < vector < ll > > matrix;
+	typedef vector < ll > vll;
+
+	#define pb push_back
+	#define debug(x) cout << (#x) << " is " << (x) << endl
+	#define fast_io() ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+	const ll mod = 1e9 + 7;
+	const ll inf = LLONG_MAX;
+	const ll N = 1e5 + 10;
+	string  as;
+	int sat_in() {
+		for (int i = 0; i < as.size(); ++i) {
+			if (as[i] == '1') {
+				int d = i;
+				return i;
+			}
+		}
+		return -1;
+	}
+	int end_in() {
+		for (int i = as.size() -1; i > -1; --i) {
+			if (as[i] == '1') {
+				int f = i;
+				return i;
+			}
+		}
+		return -1;
+	}
+	int main() {
+	    fast_io();
+	    int tet; 
+	    cin >> tet;
+	    while (tet--) {
+	    	int sum = 0;
+	    	cin >> as;
+	    	int g = sat_in();
+	    	int h = end_in();
+	    	for (int i = g; i < h; ++i) {
+	    		if (as[i] == '0') {
+	    			++sum;
+	    		}
+	    	}
+	    	cout << sum << "\n"; 
+	    }
+	}
+
+ 
--- codeforces/1304A.cpp
+++ codeforces/1304A.cpp
@@ -1,44 +1,43 @@
 /*
 RAone00
-Rohit sharma
+Rohit Sharma
 */
- 
+
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 typedef long long ll;
 typedef long double ld;
 typedef pair < ll, ll > pll;
 typedef vector < vector < ll > > matrix;
 typedef vector < ll > vll;
- 
+
 #define pb push_back
 #define debug(x) cout << (#x) << " is " << (x) << endl
 #define fast_io() ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0)
- 
+
 const ll mod = 1e9 + 7;
 const ll inf = LLONG_MAX;
 const ll N = 1e5 + 10;
-bool comp(int a, int b) 
-{ 
-    return (a < b); 
-} 
- 
+
+void solve() {
+    int a, b, x, y, d, f;
+    cin >> x >> y >> a >> b;
+    d = y - x;
+    f = a + b;
+    if (d % f) {
+    	cout << "-1\n";
+    } else {
+    	cout << (d / f) << "\n";
+    }
+}
+
 int main() {
     fast_io();
-    int num;
-    cin >> num;
-    while (num--) {
-        int a, b, c, n;
-        cin >> a >> b >> c >> n;
-        int mx = max({a, b, c}, comp);
-        int d = mx - a;
-        int f = mx - b;
-        int g = mx - c;
-        int l = n - (d + f + g);
-        if (l > 0 and l % 3){
-            cout << "NO\n";
-        } else cout << "YES\n";
+    ll tc;
+    cin >> tc;
+    for (ll i = 1; i <= tc; ++i) {
+        solve();
     }
     return 0;
 }

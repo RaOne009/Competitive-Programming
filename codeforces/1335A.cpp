cbf85d96dc1607ef0b4998740b2356983d151589
Rohit Sharma <rs1257933@gmail.com>
2020-04-21 23:23:41 +0530

    huhu

---
+++ codeforces/1335A.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+ 
+#define ff              first
+#define ss              second
+#define ll             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define w(x)            int x; cin >> x; while (x--) 
+#define pw(b,p)         pow(b,p) + 0.1
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+ 
+ 
+void c_p_c()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+ 
+int32_t main()
+{
+	c_p_c();
+	w(x) {
+		int a;
+		cin >> a;
+		if (a > 2) {
+			cout << (a - 1) / 2 << "\n";
+		} else cout << "0\n";
+		
+	}
+	return 0;
+}
